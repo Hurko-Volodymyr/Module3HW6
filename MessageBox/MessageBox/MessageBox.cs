903612e068a672cc91d4362db3709291fcc6ee99
Hurko Volodymyr <92858780+Hurko-Volodymyr@users.noreply.github.com>
2022-10-25 20:52:06 +0300

    Second fix

--- MessageBox/MessageBox/MessageBox.cs
+++ MessageBox/MessageBox/MessageBox.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Threading;
-
-namespace MessageBox
+﻿namespace MessageBox
 {
-    internal class MessageBox
+    public class MessageBox
     {
+        public event Action<State>? OnClose;
         public async void Open()
         {
             Console.WriteLine("Window has opened");
             await Task.Delay(3000);
             Console.WriteLine("Window has closed by user");
+            OnClose?.Invoke(GetRandomStatus());
         }
 
-        internal State GetRandomStatus()
+        public State GetRandomStatus()
         {
             var rnd = new Random().Next(0, 2);
             if (rnd == 1)
@@ -22,6 +21,18 @@ namespace MessageBox
             else
             {
                 return State.Cancel;
+            }
+        }
+
+        public void EventHandler(State state)
+        {
+            if (state == State.Ok)
+            {
+                Console.WriteLine("Operation is succesfull");
+            }
+            else
+            {
+                Console.WriteLine("Operation has denied");
             }
         }
     }
--- MessageBox/MessageBox/Program.cs
+++ MessageBox/MessageBox/Program.cs
@@ -1,28 +1,12 @@
 ﻿namespace MessageBox
 {
-    internal class Program
+    public class Program
     {
-        private event Action<State>? OnClose;
-
         public static void Main(string[] args)
         {
             var messageBox = new MessageBox();
-            var program = new Program();
+            messageBox.OnClose += messageBox.EventHandler;
             messageBox.Open();
-            program.OnClose = program.EventHandler;
-            program.OnClose?.Invoke(messageBox.GetRandomStatus());
-        }
-
-        private void EventHandler(State state)
-        {
-            if (state == State.Ok)
-            {
-                Console.WriteLine("Operation is succesfull");
-            }
-            else
-            {
-                Console.WriteLine("Operation has denied");
-            }
         }
     }
 }
